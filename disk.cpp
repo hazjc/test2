382502698efb9d64fb1f2edf5534f802f6142af2
Harrison <harrisoncoulter05@icloud.com>
2026-02-18 21:04:32 +0000

    Implement Disk::distance() using euclidean distance

--- disk.cpp
+++ disk.cpp
@@ -14,6 +14,8 @@ void Disk::move(double dx, double dy){
 
 double Disk::distance(Disk& other){
 
-    // HINT: COMPLETE THE CALCULATION OF THE DISTANCE BETWEEN this DISK AND THE DISK other
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return sqrt(dx*dx + dy*dy);
 
 }
